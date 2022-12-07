ab69208857b71929af2945fd66dc79489c1dbc75
hwangduli <hwangduli0515@gmail.com>
2022-12-07 23:36:16 +0900

    객체 지향 쿼리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,8 +3,12 @@ package hellojpa;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class JpaMain {
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -15,6 +19,10 @@ public class JpaMain {
 
         try {
 
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
 
             tx.commit();
 
